fbd5a3634051dc0f0b5cbb2842efc833993b2456
Zurard <zurard07@gmail.com>
2026-02-15 20:13:42 +0530

    done with parser.ts

---
+++ packages/backend/src/shell/parser.ts
@@ -0,0 +1,20 @@
+// need to parse the command in its individual components and then execute it based on the command type and arguments
+
+export const parseCommand = (command: string) => {
+  // i need to convert the command string into a json object with the command type and its arguments
+  const [cmd, ...args] = command.trim().split(/\s+/);
+
+  if (!cmd) {
+    console.log("No command entered");
+    return { parsedCommand: null };
+  }
+  const parsedCommand = {
+    cmd,
+    args,
+  };
+  console.log("Parsed Command:", parsedCommand);
+  return {
+    parsedCommand,
+  };
+};
+
